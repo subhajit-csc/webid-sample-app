7e1976e9f3f494bec8ee458c359366957c6d46fa
Subhajit Manna <subhajit-csc@users.noreply.github.com>
2021-12-06 20:18:07 +0530

    package refactor

---
+++ src/main/java/org/test/auth/oidc/webidsampleapp/WebIdSampleAppApplication.java
@@ -0,0 +1,13 @@
+package org.test.auth.oidc.webidsampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebIdSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebIdSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/test/auth/oidc/webidsampleapp/WebidSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.auth.oidc.webidsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebidSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
